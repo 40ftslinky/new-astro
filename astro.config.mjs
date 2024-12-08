830d39a34e8afe882cd767a3ce65250b6f04f36e
40ftslinky <jez@40ftslinky.com>
2024-12-08 14:07:50 +1100

    undo config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://40ftslinky.github.io',
-    base: 'new-astro',
-    trailingSlash: 'always',
+    // base: 'new-astro',
+    // trailingSlash: 'always',
 });
